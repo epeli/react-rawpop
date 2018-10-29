bb6ae6e1b0847f3e3162d595f58926b853d842db
Esa-Matti Suuronen <esa-matti@suuronen.org>
2018-10-29 20:00:23 +0200

    Add Typescript types for focus-trap

--- src/focus-trap.d.ts
+++ src/focus-trap.d.ts
@@ -1,4 +1,29 @@
 declare module "focus-trap" {
-    const foo: any;
-    export default foo;
+    interface GetDOMNode {
+        (): HTMLElement;
+    }
+
+    interface TrapOptions {
+        onActivate?: Function;
+        onDeactivate?: Function;
+        escapeDeactivates?: boolean;
+        clickOutsideDeactivates?: boolean;
+        returnFocusOnDeactivate?: boolean;
+
+        initialFocus?: string | HTMLElement | GetDOMNode;
+        fallbackFocus?: string | HTMLElement | GetDOMNode;
+    }
+
+    interface Trap {
+        activate(options: {onActivate?: Function}): Trap;
+        deactivate(options: {
+            returnFocus?: boolean;
+            onDeactivate?: Function;
+        }): Trap;
+        pause(): Trap;
+        unpause(): Trap;
+    }
+
+    function createFocusTrap(element: HTMLElement, options: TrapOptions): Trap;
+    export default createFocusTrap;
 }
